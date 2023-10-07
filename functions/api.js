e9af480c65b64d18bcac9d8387216f08a4e596ed
Mirfozil <mirfoziln@gmail.com>
2023-10-07 15:21:02 +0500

    about qoshildi

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import serverless from 'serverless-http';
+import path from 'path'
 const app = express();
+const __dirname = path.resolve()
 
 app.get('/', (req,res) => {
     res.send('Hush kelibsiz')
@@ -15,5 +17,9 @@ app.get('/all', (req,res) => {
             3: "listening music"
         }
     })
+})
+
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 })
 export const handler = serverless(app);
